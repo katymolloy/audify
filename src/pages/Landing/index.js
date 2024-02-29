3625ab39d45325508181a3a5701f2edb9fc39dbc
Katy Molloy <katyxmolloy@gmail.com>
2024-02-29 11:49:29 -0500

    Some work done on landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,15 +1,31 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa6";
+import './style.scss';
 
 export default function LandingPage() {
     return (
-        <>
-            <h1>Landing page works!</h1>
-            <Link to="/register" >
-                Register
-            </Link>
-            <Link to="/login" >
-                Login
-            </Link>
-        </>
+        <div className="container landingContainer">
+            <div>
+                <h1>AUDIFY</h1>
+                <h2>Tagline here idfk</h2>
+                <hr></hr>
+                <div className="signInButton">
+                    <Link to="/login">
+                        Sign In
+                    </Link><FaArrowRight />
+                </div>
+                <div className="createUserLink">
+                    Don't have an account?
+                    <Link to="/register" >
+                        Create one now
+                    </Link>
+                </div>
+            </div>
+
+            <div>
+                {/* scrolling album work here */}
+            </div>
+        </div>
+
     )
 }

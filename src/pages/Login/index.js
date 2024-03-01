5db028e4b5b658bb22809d3c37ca4a2e5dedf752
Katy Molloy <katyxmolloy@gmail.com>
2024-03-01 15:17:32 -0500

    States added in login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -7,21 +7,33 @@ export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
 
-const login = (e) =>{
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        const user = userCredential.user
-    })
-}
+    const login = (e) => {
+        e.preventDefault();
+        console.log('heyyy', email, password)
+        // signInWithEmailAndPassword(auth, email, password)
+        // .then((userCredential) => {
+        //     const user = userCredential.user
+        // })
+    }
 
     return (
         <div className="container loginContainer">
             <h1>Login</h1>
-            <form>
+            <form className="loginForm">
                 <div>
-
+                    <label>Email <input 
+                    type="text"
+                    onChange={(e) => setEmail(e.target.value)}
+                    ></input></label>
                 </div>
+                <div>
+                    <label>Password <input 
+                    type="text"
+                    onChange={(e) => setPassword(e.target.value)}
+                    ></input></label>
+                </div>
+
+                <button type="submit" onClick={login}>Login</button>
             </form>
         </div>
     )

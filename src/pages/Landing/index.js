eafd09dae9bad397982b00480e15c1c23e3030ec
Matthew Gooding <goodmatt190@gmail.com>
2024-03-01 18:39:15 -0500

    Landing page styled

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -5,24 +5,35 @@ import './style.scss';
 export default function LandingPage() {
     return (
         <div className="container landingContainer">
-            <div>
-                <h1>AUDIFY</h1>
-                <h2>Tagline here idfk</h2>
-                <hr></hr>
+            <div className="info">
+                <span className="logo">
+                    <img src="/images/AudifyLogo.png" alt="Audify Logo"></img>
+                    <div className="column">
+                        <p className="logoTitle">AUDIFY</p>
+                        <p className="logoTag">Unleash Your Thoughts</p>
+                    </div>
+                </span>
+                <h1 className="">Blah Blah Blah Blah</h1>
+                <h2>Album Review Platform</h2>
                 <div className="signInButton">
                     <Link to="/login">
                         Sign In
                     </Link><FaArrowRight />
                 </div>
-                <div className="createUserLink">
+                <div className="column createAccount">
                     Don't have an account?
                     <Link to="/register" >
                         Create one now
                     </Link>
                 </div>
+                <span className="spotifyTag">
+                    <img src="/images/spotifyLogo.png" alt="Spotify Logo"></img>
+                    <p>Powered By Spotify</p>
+                </span>
             </div>
 
-            <div>
+
+            <div className="albums">
                 {/* scrolling album work here */}
             </div>
         </div>

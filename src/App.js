d5efbe9c241cc70a773f3fa4d0fb54bb06a3f71c
Vas <vasili2142@gmail.com>
2024-03-02 15:52:38 -0500

    Add dark mode toggle to landing page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
 
 function App() {
+
   return (
     <Router>
      <Routes>
-      <Route path='/' element={<LandingPage />} />
+      <Route path='/' element={<LandingPage />}/>
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
      </Routes>
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,43 +1,50 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa6";
-import './style.scss';
+import { MdDarkMode } from "react-icons/md";
+
+import "./style.scss";
 
 export default function LandingPage() {
-    return (
-        <div className="container landingContainer">
-            <div className="info">
-                <span className="logo">
-                    <img src="/images/AudifyLogo.png" alt="Audify Logo"></img>
-                    <div className="column">
-                        <p className="logoTitle">AUDIFY</p>
-                        <p className="logoTag">Unleash Your Thoughts</p>
-                    </div>
-                </span>
-                <h1 className="">Blah Blah Blah Blah</h1>
-                <h2>Album Review Platform</h2>
-                <div className="signInButton">
-                    <Link to="/login">
-                        Sign In
-                        <FaArrowRight />
-                    </Link>
-                </div>
-                <div className="column createAccount">
-                    Don't have an account?
-                    <Link to="/register" >
-                        Create one now
-                    </Link>
-                </div>
-                <span className="spotifyTag">
-                    <img src="/images/spotifyLogo.png" alt="Spotify Logo"></img>
-                    <p>Powered By Spotify</p>
-                </span>
-            </div>
+  const [isDark, setIsDark] = useState('light');
 
+  return (
+    <div className="container landingContainer" id={isDark}>
+      <div className="info">
+        <span className="logo">
+          <img src="/images/AudifyLogo.png" alt="Audify Logo"></img>
 
-            <div className="albums">
-                {/* scrolling album work here */}
-            </div>
+          <div className="column">
+            <p className="logoTitle">AUDIFY</p>
+            <p className="logoTag">Unleash Your Thoughts</p>
+          </div>
+
+          <button
+            className="dark-mode-toggle"
+            onClick={() => setIsDark(isDark === 'light' ? 'dark' : 'light')}
+          >
+            <MdDarkMode />
+          </button>
+        </span>
+        <h1 className="">Blah Blah Blah Blah</h1>
+        <h2>Album Review Platform</h2>
+        <div className="signInButton">
+          <Link to="/login">
+            Sign In
+            <FaArrowRight />
+          </Link>
         </div>
+        <div className="column createAccount">
+          Don't have an account?
+          <Link to="/register">Create one now</Link>
+        </div>
+        <span className="spotifyTag">
+          <img src="/images/spotifyLogo.png" alt="Spotify Logo"></img>
+          <p>Powered By Spotify</p>
+        </span>
+      </div>
 
-    )
+      <div className="albums">{/* scrolling album work here */}</div>
+    </div>
+  );
 }

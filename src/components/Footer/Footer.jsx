8f050b0eccac0a40872cf1df29d27729bc16849a
Vas <vasili2142@gmail.com>
2024-03-03 22:15:06 -0500

    Add responsive design to landing page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,49 +2,34 @@
 
 import { FaSpotify } from "react-icons/fa";
 
-import './footer-styles.scss';
-
+import "./footer-styles.scss";
 
 export default function Footer() {
+  return (
+    <footer className="footer-main">
+      <div className="shoutout-logo">
+        <div className="audify-logo">Audify</div>
+        <div className="shoutout">
+          <FaSpotify />
+          Powered By Spotify
+        </div>
+      </div>
 
-    return (
+      <div className="links-section">
+        <div className="title">Quick Links</div>
 
-        <>
+        <a href="#">Leave a Review</a>
 
-            <footer className="footer-main">
+        <a href="#">My Account</a>
 
-                <div className="shoutout-logo">
-                    <div className="audify-logo">
-                        Audify
-                    </div>
-                    <div className='shoutout'>
-                        <FaSpotify />Powered By Spotify hello world
-                    </div>
-                </div>
+        <a href="#">About Us</a>
 
+      </div>
 
-                <div className="links-section">
-                    <div className = "title">Quick Links</div>
-
-                    <a href='#'>Leave a Review</a>
-
-                    <a href='#'>My Account</a>
-
-                    <a href='#'>About Us</a>
-
-                    <a href='#'>Sign Up</a>
-                </div>
-
-
-                <div className='copyright'>
-                    <div>Created By Group 9</div>
-                    <div>&copy; 2024 Audify</div>
-                </div>
-
-            </footer>
-
-        </>
-
-    );
-
+      <div className="copyright">
+        <div>Created By Group 9</div>
+        <div>&copy; 2024 Audify</div>
+      </div>
+    </footer>
+  );
 }
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -34,20 +34,18 @@ export default function LandingPage() {
           </span>
           <h1 className="">Blah Blah Blah Blah</h1>
           <h2>Album Review Platform</h2>
-          <div className="signInButton">
-            <Link to="/login">
-              Sign In
-              <FaArrowRight />
-            </Link>
+          <div className="signIn-signUp-container">
+            <div className="signInButton">
+              <Link to="/login">
+                Sign In
+                <FaArrowRight />
+              </Link>
+            </div>
+            <div className="column createAccount">
+              Don't have an account?
+              <Link to="/register">Create one now</Link>
+            </div>
           </div>
-          <div className="column createAccount">
-            Don't have an account?
-            <Link to="/register">Create one now</Link>
-          </div>
-          <span className="spotifyTag">
-            <img src="/images/spotifyLogo.png" alt="Spotify Logo"></img>
-            <p>Powered By Spotify</p>
-          </span>
         </div>
         <div className="albums">{/* scrolling album work here */}</div>
       </div>

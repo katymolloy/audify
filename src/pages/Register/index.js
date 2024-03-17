605dc25523fe3f61e225e950b523a1f1c4212872
Vas <vasili2142@gmail.com>
2024-03-17 16:36:07 -0400

    Add error validation

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
-import './register.scss';
+import "./register.scss";
 import { db } from "../../firebase";
 
 import { redirectUri, clientId } from "../../util/spotify";
@@ -9,133 +9,170 @@ import { doc, setDoc, getDocs, collection } from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
-    const [displayName, setDisplayName] = useState('');
-    const [profilePic, setProfilePic] = useState('');
-    const [unavailUsers, setUnavailUsers] = useState([])
-    const [errorMsg, setErrorMsg] = useState([])
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const [unavailUsers, setUnavailUsers] = useState([]);
+  const [errorMsg, setErrorMsg] = useState([]);
 
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    const unavailableUsernames = [];
+    const retrieveUsernames = async () => {
+      const querySnapshot = await getDocs(collection(db, "usernames"));
+      querySnapshot.forEach((doc) => {
+        unavailableUsernames.push(doc.id);
+      });
+      console.log(unavailableUsernames);
 
-    useEffect(() => {
-        const unavailableUsernames = [];
-        const retrieveUsernames = async () => {
-            const querySnapshot = await getDocs(collection(db, "usernames"));
-            querySnapshot.forEach((doc) => {
-                unavailableUsernames.push(doc.id)
-            
-            });
-            console.log(unavailableUsernames)
+      return unavailableUsernames;
+    };
+    retrieveUsernames();
+  }, []);
 
-            return unavailableUsernames;
+  const writeToDb = async (userId) => {
+    try {
+      await setDoc(doc(db, "users", userId), {
+        uid: userId,
+        username: username,
+        display: displayName,
+        pfp: profilePic,
+      });
+
+      await setDoc(doc(db, "usernames", username), {
+        username: username,
+        uid: userId,
+      });
+      console.log("Data added successfully");
+
+      navigate("/home");
+    } catch (error) {
+      console.log(`Error writing to database: ${error}`);
+    }
+  };
+
+  const registerUser = async () => {
+    if (errorMsg.length === 0) {
+      await createUserWithEmailAndPassword(auth, email, password).then(
+        (userCredential) => {
+          const user = userCredential.user;
+          writeToDb(user.uid);
         }
-        retrieveUsernames();
-    }, [])
-
-
-    const writeToDb = async (userId) => {
-        try {
-            await setDoc(doc(db, 'users', userId), {
-                uid: userId,
-                username: username,
-                display: displayName,
-                pfp: profilePic,
-            });
-
-            await setDoc(doc(db, 'usernames', username), {
-                username: username,
-                uid: userId,
-            });
-            console.log('Data added successfully')
-
-            navigate('/home');
-
-        } catch (error) {
-            console.log(`Error writing to database: ${error}`)
-        }
+      );
     }
+  };
 
+  // const handleImageSubmit = () => {
 
+  // }
 
-    const registerUser = async () => {
-        if (errorMsg.length === 0) {
-            await createUserWithEmailAndPassword(auth, email, password)
-                .then((userCredential) => {
-                    const user = userCredential.user
-                    writeToDb(user.uid);
-                })
-        }
+  // const authorizeSpotify = () => {
+  //     window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+  //     setAuthSpotify(true)
+  // }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const validate = [];
+
+    // check for invalid/missing data
+    if (username.length < 5) {
+      validate.push("Please enter a valid username with at least 5 characters");
     }
-
-
-    const handleImageSubmit = () => {
-
+    if (displayName.length < 5) {
+      validate.push(
+        "Please enter a valid display name with at least 5 characters"
+      );
     }
-
-
-    // const authorizeSpotify = () => {
-    //     window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
-    //     setAuthSpotify(true)
-    // }
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-        registerUser();
+    if (email.length < 8) {
+      validate.push("Please enter a valid email with 8, or more, characters");
     }
+    if (password.length < 5) {
+      validate.push("Please enter a password with at least 5 characters");
+    }
+    setErrorMsg(validate);
 
-    return (
-        <div className="container registerContainer">
-            <Link to={'/'}>Back</Link>
-            <h1>Create Account</h1>
-            <form className="loginForm">
-                <div className="errorBox">
-                    {errorMsg && errorMsg.forEach((error) => error)}
-                </div>
+    // run when no data is invalid/missing
+    if (validate.length === 0) {
+      registerUser();
+    }
+  };
 
+  return (
+    <div className="container registerContainer">
+      <Link to={"/"}>Back</Link>
+      <h1>Create Account</h1>
+      <form className="loginForm">
+        {errorMsg.length > 0 && (
+            <div className="errorBox">
+                Invalid data:
+                <ul>
+                    {errorMsg.map((error, index) => (
+                        <li key={index}>{error}</li>
+                    ))}
+                </ul>
+            </div>            
+        )}
 
-                <div>
-                    <label>Username <input
-                        type="text"
-                        onChange={(e) => setUsername(e.target.value)}
-                    ></input></label>
-                </div>
-                <div>
-                    <label>Display Name <input
-                        type="text"
-                        onChange={(e) => setDisplayName(e.target.value)}
-                    ></input></label>
-                </div>
+        <div>
+          <label>
+            Username{" "}
+            <input type="text" onChange={(e) => setUsername(e.target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Display Name{" "}
+            <input
+              type="text"
+              onChange={(e) => setDisplayName(e.target.value)}
+            ></input>
+          </label>
+        </div>
 
-                <div>
+        {/* <div>
                     <label>Profile Picture <input
                         type="file"
                         onChange={handleImageSubmit}
                     ></input></label>
-                </div>
+                </div> */}
 
-                <div>
-                    <label>Email <input
-                        type="text"
-                        onChange={(e) => setEmail(e.target.value)}
-                    ></input></label>
-                </div>
-                <div>
-                    <label>Password <input
-                        type="text"
-                        onChange={(e) => setPassword(e.target.value)}
-                    ></input></label>
-                </div>
-                <div>Use of Audify requires a Spotify account. Not yet a Spotify user? Sign up <a href="https://www.spotify.com/us/signup" target="_blank">here!</a></div>
-
-                {/* <button type="button" onClick={authorizeSpotify}>Link Spotify</button> */}
-                {/* {authSpotify && */}
-                <button type="submit" onClick={submitHandler}>Create Account</button>
-                {/* } */}
-
-            </form>
+        <div>
+          <label>
+            Email{" "}
+            <input
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </label>
         </div>
-    )
+        <div>
+          <label>
+            Password{" "}
+            <input
+              type="text"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </label>
+        </div>
+        <div>
+          Use of Audify requires a Spotify account. Not yet a Spotify user? Sign
+          up{" "}
+          <a href="https://www.spotify.com/us/signup" target="_blank">
+            here!
+          </a>
+        </div>
+
+        {/* <button type="button" onClick={authorizeSpotify}>Link Spotify</button> */}
+        {/* {authSpotify && */}
+        <button type="submit" onClick={submitHandler}>
+          Create Account
+        </button>
+        {/* } */}
+      </form>
+    </div>
+  );
 }
